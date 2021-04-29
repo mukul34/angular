b56d55fb39adc0f4f9179a72c6edc8bf9af67ef1
mukul34 <sirohi.mukul1999@gmail.com>
2021-04-29 14:13:39 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Assignment1';
+  showPara = false;
+  para = [
+    {
+      data: 'Lorem20 ipsum',
+    },
+  ];
+  userInput: string="";
+  log:number=0;
+  toggleDisplay() {
+    this.showPara = !this.showPara;
+    this.log+=1;
+  }
+  changeInput(value: string) {
+    this.userInput = value;
+  }
+  addData() {
+    if (this.userInput != '') {
+      this.para.push({ data: this.userInput });
+    }
+    this.showPara=true;
+  }
+  displayButtonName(){
+    if( this.showPara==false){
+      return "Display Details";
+    }
+    else{
+      return "Hide Details";
+    }
+  }
 }
